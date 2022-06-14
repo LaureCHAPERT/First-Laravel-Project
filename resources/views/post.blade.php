9b2a39815acaa9f86e7ef8a2d437b8cf72887888
LaureCHAPERT <laure.chapert@gmail.com>
2022-06-14 15:41:36 +0200

    Make a route and LInk to It AND use slug

---
+++ resources/views/post.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+    <title>My blog</title>
+    <link rel="stylesheet" href="/app.css">
+</head>
+<body>
+    <article>
+        <?= $post; ?>
+    </article>
+</body>
+</html>
---
+++ resources/views/posts.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+    <title>My blog</title>
+    <link rel="stylesheet" href="/app.css">
+</head>
+<body>
+    <article>
+        <h1> <a href="/posts/my-first-post"> My first post </a></h1>
+        <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error, deserunt velit. Praesentium hic soluta itaque reprehenderit, doloremque laboriosam totam incidunt porro quasi assumenda veritatis optio. Possimus unde quas alias vitae?</p>
+    </article>
+    <article>
+        <h1>  <a href="/posts/my-second-post"> My second post</a> </h1>
+        <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error, deserunt velit. Praesentium hic soluta itaque reprehenderit, doloremque laboriosam totam incidunt porro quasi assumenda veritatis optio. Possimus unde quas alias vitae?</p>
+    </article>
+    <article>
+        <h1>  <a href="/posts/my-third-post"> My third post</a> </h1>
+        <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error, deserunt velit. Praesentium hic soluta itaque reprehenderit, doloremque laboriosam totam incidunt porro quasi assumenda veritatis optio. Possimus unde quas alias vitae?</p>
+    </article>
+</body>
+</html>
